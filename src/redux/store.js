beda52682d2c848d0d23b36c16d3f4687d74ede5
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-08-26 14:26:13 -0300

    iniciando projeto

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,13 @@
-// configure aqui sua store
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
